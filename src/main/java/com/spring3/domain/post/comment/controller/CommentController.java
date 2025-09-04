196564114b95ab170187cf4df84baf5407016d81
chacha86 <taejincha0619@gmail.com>
2025-09-04 15:25:28 +0900

    work

--- src/main/java/com/spring3/domain/post/comment/controller/CommentController.java
+++ src/main/java/com/spring3/domain/post/comment/controller/CommentController.java
@@ -10,9 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -62,7 +60,7 @@ public class CommentController {
         return "post/comment/modify";
     }
 
-    @PostMapping("/posts/{postId}/comments/{commentId}/modify")
+    @PutMapping("/posts/{postId}/comments/{commentId}/modify")
     @Transactional
     public String doModify(
             @PathVariable Long postId,
@@ -79,7 +77,7 @@ public class CommentController {
 
     @GetMapping("/posts/{postId}/comments/{commentId}/delete")
     @Transactional
-    public String delete(
+    public String doDelete(
             @PathVariable Long postId,
             @PathVariable Long commentId
     ) {
