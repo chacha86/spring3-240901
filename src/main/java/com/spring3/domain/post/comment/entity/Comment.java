b97d754f82ff05b69e7ba374033a8b43f3d08a46
chacha86 <taejincha0619@gmail.com>
2025-09-04 10:32:15 +0900

    work

---
+++ src/main/java/com/spring3/domain/post/comment/entity/Comment.java
@@ -0,0 +1,18 @@
+package com.spring3.domain.post.comment.entity;
+
+import com.spring3.domain.post.post.entity.Post;
+import com.spring3.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+
+    private String content;
+    @ManyToOne
+    private Post post;
+}
--- src/main/java/com/spring3/domain/post/post/entity/Post.java
+++ src/main/java/com/spring3/domain/post/post/entity/Post.java
@@ -1,9 +1,16 @@
 package com.spring3.domain.post.post.entity;
 
+import com.spring3.domain.post.comment.entity.Comment;
 import com.spring3.global.jpa.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -11,6 +18,9 @@ import lombok.NoArgsConstructor;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY)
+    private List<Comment> comments = new ArrayList<>();
 
     public Post(String title, String content) {
         this.title = title;
@@ -20,5 +30,12 @@ public class Post extends BaseEntity {
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public Comment addComment(String content) {
+        Comment comment = new Comment(content, this);
+        this.comments.add(comment);
+
+        return comment;
     }
 }
