48624df70c09a444b70409cdda69063ad175bea2
chacha86 <taejincha0619@gmail.com>
2025-09-04 11:50:58 +0900

    work

--- src/main/java/com/spring3/domain/post/comment/controller/CommentController.java
+++ src/main/java/com/spring3/domain/post/comment/controller/CommentController.java
@@ -10,8 +10,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -33,16 +31,9 @@ public class CommentController {
     @Transactional
     public String write(
             @PathVariable Long postId,
-            @Valid CommentWriteForm form,
-            BindingResult bindingResult,
-            Model model
+            @Valid CommentWriteForm form
     ) {
         Post post = postService.findById(postId).get();
-
-        if(bindingResult.hasErrors()) {
-            model.addAttribute("post", post);
-            return "post/detail";
-        }
 
         postService.writeComment(post, form.getContent());
         return "redirect:/posts/" + postId;
